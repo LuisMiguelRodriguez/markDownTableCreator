76479142f9142d12e2b0dc5046b6c828cfd7f9e9
LuisMiguelRodriguez <luis.the.coder@gmail.com>
2018-02-02 08:00:28 -0800

    removed unwanted code and update strings to template literals

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var fs = require("fs");
 var path = require("path");
 
+// text to parse thru
 var text = `
 d----- 1/7/2018 9:06 PM 01-Hangman
 d----- 1/7/2018 9:06 PM 02-CaptainPlanet 
@@ -29,36 +30,38 @@ d----- 1/7/2018 9:06 PM 24-RecursiveFibonacci
 d----- 1/7/2018 9:06 PM 25-StringCalculator 
 `;
 
-var newArray = text.split("d----- 1/7/2018 9:06 PM ");
+// created array that holds all project name with numbers
+// Using split to seperate unwanted text
+var newArray = text.split("d----- 1/7/2018 9:06 PM ")
 
+// go through array and remove new line
+// character added by the template literal
+for (var i = 0; i < newArray.length; i++) {
+  newArray[i] = newArray[i].slice(0, -1);
+}
+
+// declaring an ampty array that
+// will hold project names without numbers
 var finishedArray = [];
 
+// created an array of project names without numbers
+// used slice to remove the first 3 letters from each
+// item in the array
 for (var i = 0; i < newArray.length; i++) {
   console.log(newArray[i]);
   finishedArray.push(newArray[i].slice(3));
 }
 
-var bigString = newArray.join("");
+// var bigString = newArray.join("");
 
 // console.log(bigString)
 
-// console.log(newArray);
+// console.log(finishedArray);
 
 var linkArray = [];
 
-for (var i = 0; i < newArray.length; i++) {
-  newArray[i] = newArray[i].slice(0, -1);
-}
-
-// console.log(newArray);
-
-// console.log(finishedArray);
-
-for (var i = 0; i < finishedArray.length; i++) {
-  finishedArray[i] = finishedArray[i].slice(0, -1);
-}
-
-// console.log(finishedArray);
+// created object to hold both project names
+// with and without numbers
 
 // var arrOfObjects = {};
 
@@ -68,18 +71,11 @@ for (var i = 0; i < finishedArray.length; i++) {
 //     link: finishedArray[i]
 //   };
 // }
-
 // console.log(arrOfObjects);
 
-//  var arrName1 = arr1[i].name
-//  var arrLink1 = arr1[i].link
-
-//  var arrName2 = arr2[i].name
-//  var arrLink2 = arr2[i].link
-
-//  var arrName3 = arr3[i].name
-//  var arrLink3 = arr3[i].link
-
+// seperating large array into 3 
+// which represent each day of class
+// for the week
 var day1 = newArray.slice(1, 7);
 console.log(day1);
 
@@ -89,74 +85,61 @@ console.log(day2);
 var day3 = newArray.slice(15, 25);
 console.log(day3);
 
-// `|[${arrName1}](01-Class-Content/02-css-bootstrap/01-Activities/${arrLink1})|`
-// `|[${arrName2}](01-Class-Content/02-css-bootstrap/01-Activities/${arrLink2})|`
-// `|[${arrName3}](01-Class-Content/02-css-bootstrap/01-Activities/${arrLink3})|`
 
 // (01-Class-Content/02-css-bootstrap/01-Activities/${link})
 
 function createTable(arr1, arr2, arr3, unit) {
+
   console.log(arr1);
   console.log(arr2);
   console.log(arr3);
   console.log(unit);
 
+  // Grab folder name to create a file path
+  var folderName = '05-timers'
+
+  // compare each day to see what the
+  // height of the table will be
   var loopCount = Math.max(arr1.length, arr2.length, arr3.length);
 
-  console.log(loopCount);
-
+  // var to hold completed string
+  // initialized with table header data
   var longString =
     "## Unit 5 - JavaScript / jQuery Review, Timers, & Debugger \n";
   longString +=
     "Day # 1 Activities|Day # 2 Activities|Day #3 Activities|Homework|Videos|\n";
   longString += "|---|---|---|---|---|\n";
 
-  fs.appendFile("pleaseWork.txt", longString, function(err) {
-    if (err) throw err;
-    console.log("Saved!");
-  });
-
+  // loop that will repeat as many times as the highest day
+  // with activities which sets the height
   for (var i = 0; i < loopCount; i++) {
+
+    // Array Parm #1
     var arrName1 = arr1[i] ? arr1[i] : "---";
     var arrLink1 = arr1[i] ? arr1[i] : "---";
 
+    // Array Parm #2
     var arrName2 = arr2[i] ? arr2[i] : "---";
     var arrLink2 = arr2[i] ? arr2[i] : "---";
 
+    // Array Parm #3
     var arrName3 = arr3[i] ? arr3[i] : "---";
     var arrLink3 = arr3[i] ? arr3[i] : "---";
 
-    console.log(arrName1 + arrName2 + arrName3);
 
-    longString +=
-      "|" +
-      " " +
-      "[" +
-      arrName1 +
-      "](01-Class-Content/02-css-bootstrap/01-Activities/" +
-      arrLink1 +
-      ")" +
-      " ";
-    longString +=
-      "|" +
-      " " +
-      "[" +
-      arrName2 +
-      "](01-Class-Content/02-css-bootstrap/01-Activities/" +
-      arrLink2 +
-      " )" +
-      " ";
-    longString +=
-      "|" +
-      " " +
-      "[" +
-      arrName3 +
-      "](01-Class-Content/02-css-bootstrap/01-Activities/" +
-      arrLink3 +
-      ")|\n";
+    longString += `|[${arrName1}](01-Class-Content/${folderName}/01-Activities/${arrLink1})`
+    longString += `|[${arrName2}](01-Class-Content/${folderName}/01-Activities/${arrLink2})`
+    longString += `|[${arrName3}](01-Class-Content/${folderName}/01-Activities/${arrLink3})|---|---|\n`
+
+    console.log(longString);
+
   }
 
-  console.log(longString);
+  fs.appendFile("pleaseWork.txt", longString, function (err) {
+    if (err) throw err
+    console.log("Saved!")
+  });
+
 }
 
-createTable(day1, day2, day3);
+createTable(day1, day2, day3)
